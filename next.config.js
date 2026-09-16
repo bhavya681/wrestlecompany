ce94065c01ff0bea38f713fa3d168682fbcb4895
bhavya681 <bhavyawade2@gmail.com>
2026-09-16 21:46:57 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -1,21 +1,38 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.pravatar.cc",
+      },
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+      },
+    ],
+
     domains: [
       "tnawrestling.com",
       "www.tnawrestling.com",
+
       "wrestlesquare.com",
       "www.wrestlesquare.com",
+
       "onlineworldofwrestling.com",
       "www.onlineworldofwrestling.com",
+
       "pbs.twimg.com",
       "encrypted-tbn0.gstatic.com",
       "hips.hearstapps.com",
+
       "www.si.com",
-      "chatgpt.com","user32962.na.imgto.link"
+
+      "chatgpt.com",
+
+      "user32962.na.imgto.link",
     ],
   },
 };
 
 module.exports = nextConfig;
-
